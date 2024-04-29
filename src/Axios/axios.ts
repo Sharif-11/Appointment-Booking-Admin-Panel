c6a61626fe5bc0d51391b38fcb273f341e830c88
Sharif-11 <sharifsharifulislam061@gmail.com>
2024-04-29 22:15:20 +0600

    base url changed

--- src/Axios/axios.ts
+++ src/Axios/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/api/v1/";
+export const baseURL = "http://18.138.71.5:5000/api/v1/";
 const axiosInstance = axios.create({ baseURL, withCredentials: true });
 export const setHeader = (token: string) => {
   axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
